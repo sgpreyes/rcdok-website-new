f555b67f4d26173cbbd0d6447cf0f8dadc58c514
Reana Anika Manucom <ramanucom@gmail.com>
2026-02-04 17:05:34 +0800

    Initial Starlight setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://MOCKIE26.github.io',
+    base: '/rcdok-website-new',
+
 	integrations: [
 		starlight({
 			title: 'My Docs',
