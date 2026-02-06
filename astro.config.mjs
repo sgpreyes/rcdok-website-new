882ba6615ab137ad5b4d8dc878a629abc5016caa
Kaela-4650 <154566193+Kaela-4650@users.noreply.github.com>
2026-02-06 16:35:48 +0800

    Change autogenerate directory for Member Schools

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
 				},
 				{
 					label: 'Member Schools',
-					autogenerate: { directory: 'reference' },
+					autogenerate: { directory: 'member-school' },
 				},
 				{
 					label: 'Clergy',
