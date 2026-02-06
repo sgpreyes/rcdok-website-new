43b4255d728e4472372ead7d68af3cd08bb2d00c
Kaela-4650 <154566193+Kaela-4650@users.noreply.github.com>
2026-02-06 16:47:16 +0800

    Fix autogenerate directory name for Member Schools

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
 				},
 				{
 					label: 'Member Schools',
-					autogenerate: { directory: 'member-schools' },
+					autogenerate: { directory: 'member school' },
 				},
 				{
 					label: 'Clergy',
